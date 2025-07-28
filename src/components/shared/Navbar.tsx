8c0a06be7ab0dede3b2bb4a852500d661cb655c9
jubayer718 <jubayerahmed2462@gmail.com>
2025-07-28 09:25:43 +0600

    Navbar almost done.

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -1,10 +1,30 @@
-import React from 'react';
+
 import Container from './Container';
+import Link from 'next/link';
+import { Button } from '../ui/button';
+import { Search, User } from 'lucide-react';
+import { cn } from '@/lib/utils';
+import { RiShoppingBasketLine } from "react-icons/ri";
 
 const Navbar = () => {
   return (
     <Container > 
-      <h1>This is navbar</h1>
+      <nav className='flex items-center justify-between'>
+        
+        <ul className='flex items-center gap-12'>
+          <Link href={'/'}>Home</Link>
+          <Link href={'/'}>Shop</Link>
+          <Link href={'/'}>Blogs</Link>
+          <Link href={'/'}>Content</Link>
+        </ul>
+
+        <div className='flex items-center gap-12'>
+        <Button className={cn('')} variant={'ghost'}>Login <User/> </Button>  
+        <Button className={cn('')} variant={'ghost'}><Search/> </Button>  
+        <Button className={cn('')} variant={'ghost'}><RiShoppingBasketLine/> </Button>  
+    </div>
+
+     </nav>
     </Container>
    
   );
